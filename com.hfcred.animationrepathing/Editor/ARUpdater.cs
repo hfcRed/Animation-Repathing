8ad2f2dda411edab7de7471a9b57e32cb708138e
hfcRed <elias.horn@gmx.de>
2023-07-28 23:27:48 +0200

    Updated updater

--- com.hfcred.animationrepathing/Editor/ARUpdater.cs
+++ com.hfcred.animationrepathing/Editor/ARUpdater.cs
@@ -9,7 +9,7 @@ namespace AnimationRepathing
 {
     public class ARUpdater
     {
-        public static string versionURL = "https://raw.githubusercontent.com/hfcRed/Animation-Repathing/main/version.txt";
+        public static string versionURL = "https://raw.githubusercontent.com/hfcRed/Animation-Repathing/main/com.hfcred.animationrepathing/version.txt";
         public static string downloadURL = "https://github.com/hfcRed/Animation-Repathing/releases/latest/download/Animation-Repathing.unitypackage";
         public static string outputPath = "Assets/AROutput.unitypackage";
         public static List<string> assetsToDelete = new List<string>();
