31f2f75657dba6ec07ae070e24b63af5315da3af
hfcRed <elias.horn@gmx.de>
2023-07-29 14:40:00 +0200

    Now reads version from package.json Auto updater wont delete scripts if Unity package download failed

--- com.hfcred.animationrepathing/Editor/ARUpdater.cs
+++ com.hfcred.animationrepathing/Editor/ARUpdater.cs
@@ -9,19 +9,41 @@ namespace AnimationRepathing
 {
     public class ARUpdater
     {
-        public static string versionURL = "https://raw.githubusercontent.com/hfcRed/Animation-Repathing/main/com.hfcred.animationrepathing/version.txt";
-        public static string downloadURL = "https://github.com/hfcRed/Animation-Repathing/releases/latest/download/AnimationRepathing_";
+        public static string versionURL = "https://raw.githubusercontent.com/hfcRed/Animation-Repathing/main/com.hfcred.animationrepathing/package.json";
+        public static string downloadURL = "https://github.com/hfcRed/Animation-Repathing/releases/latest/download/AnimationRepathing_v";
         public static string outputPath = "Assets/AROutput.unitypackage";
         public static List<string> assetsToDelete = new List<string>();
         public static List<string> metasToDelete = new List<string>();
 
         public static bool CheckForNewVersion()
         {
+            newestVersion = string.Empty;
+            currentVersion = string.Empty;
+
+            currentVersion = GetCurrentVersion()?.ToString();
             newestVersion = GetNewestVersion()?.ToString();
-            currentVersion = AssetDatabase.LoadAssetAtPath<TextAsset>(AssetDatabase.GUIDToAssetPath("687ec5539659fd440992163de55107d7"))?.text;
 
             if (newestVersion != null && newestVersion != currentVersion) return true;
             else return false;
+        }
+
+        [System.Serializable]
+        public class Version
+        {
+            public string version;
+        }
+
+        public static string GetCurrentVersion()
+        {
+            var editor = ScriptableObject.CreateInstance<AREditor>();
+            string path = editor.GetScriptPath();
+
+            string folderPath = Path.GetDirectoryName(path);
+            string parentFolder = Path.GetDirectoryName(folderPath);
+            string jsonText = File.ReadAllText(parentFolder + "/package.json");
+
+            Version version = JsonUtility.FromJson<Version>(jsonText);
+            return version.version;
         }
 
         public static string GetNewestVersion()
@@ -29,7 +51,10 @@ namespace AnimationRepathing
             try
             {
                 var client = new System.Net.WebClient();
-                return client.DownloadString(versionURL);
+                string jsonText = client.DownloadString(versionURL);
+
+                Version version = JsonUtility.FromJson<Version>(jsonText);
+                return version.version;
             }
             catch (System.Net.WebException e)
             {
@@ -40,17 +65,26 @@ namespace AnimationRepathing
 
         public static void UpdateTool()
         {
-            var client = new UnityWebRequest(downloadURL + "v" + newestVersion + ".unitypackage")
+            bool downloadSuccess = false;
+
+            var client = new UnityWebRequest(downloadURL + newestVersion + ".unitypackage")
             {
                 downloadHandler = new DownloadHandlerFile(outputPath)
             };
             client.SendWebRequest().completed += AA =>
             {
-                GetAssetsToDelete();
-                AssetDatabase.ImportAsset(outputPath);
-                AssetDatabase.ImportPackage(outputPath, false);
+                if (client.downloadHandler.isDone)
+                {
+                    downloadSuccess = true;
+                    GetAssetsToDelete();
+                    AssetDatabase.ImportAsset(outputPath);
+                    AssetDatabase.ImportPackage(outputPath, false);
+                }
+
                 client.Dispose();
-                DeleteAssets();
+
+                if (downloadSuccess) DeleteAssets();
+                else Debug.LogError("Animation Repathing: Download could not be completed, you may have to manually update the tool through the VRChat Creator Companion or by downloading the latest release from https://github.com/hfcRed/Animation-Repathing/releases");
             };
         }
 
