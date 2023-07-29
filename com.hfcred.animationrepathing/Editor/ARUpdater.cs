f4d1cc4d8eb8793076df5e0df1e821075f4a3b5a
hfcRed <elias.horn@gmx.de>
2023-07-29 19:20:05 +0200

    Updated updater

--- com.hfcred.animationrepathing/Editor/ARUpdater.cs
+++ com.hfcred.animationrepathing/Editor/ARUpdater.cs
@@ -1,6 +1,7 @@
 using static AnimationRepathing.ARVariables;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.Networking;
@@ -11,9 +12,10 @@ namespace AnimationRepathing
     {
         public static string versionURL = "https://raw.githubusercontent.com/hfcRed/Animation-Repathing/main/com.hfcred.animationrepathing/package.json";
         public static string downloadURL = "https://github.com/hfcRed/Animation-Repathing/releases/latest/download/AnimationRepathing_v";
-        public static string outputPath = "Assets/AROutput.unitypackage";
+        public static string packagePath = "Assets/AROutput.unitypackage";
+
         public static List<string> assetsToDelete = new List<string>();
-        public static List<string> metasToDelete = new List<string>();
+        // public static List<string> metasToDelete = new List<string>();
 
         public static bool CheckForNewVersion()
         {
@@ -65,11 +67,12 @@ namespace AnimationRepathing
 
         public static void UpdateTool()
         {
+            AssetDatabase.Refresh();
             bool downloadSuccess = false;
 
             var client = new UnityWebRequest(downloadURL + newestVersion + ".unitypackage")
             {
-                downloadHandler = new DownloadHandlerFile(outputPath)
+                downloadHandler = new DownloadHandlerFile(packagePath)
             };
             client.SendWebRequest().completed += AA =>
             {
@@ -77,8 +80,10 @@ namespace AnimationRepathing
                 {
                     downloadSuccess = true;
                     GetAssetsToDelete();
-                    AssetDatabase.ImportAsset(outputPath);
-                    AssetDatabase.ImportPackage(outputPath, false);
+
+                    AssetDatabase.Refresh();
+                    AssetDatabase.ImportAsset(packagePath);
+                    AssetDatabase.ImportPackage(packagePath, false);
                 }
 
                 client.Dispose();
@@ -91,36 +96,38 @@ namespace AnimationRepathing
         public static void GetAssetsToDelete()
         {
             assetsToDelete.Clear();
-            metasToDelete.Clear();
+            // metasToDelete.Clear();
 
             var editor = ScriptableObject.CreateInstance<AREditor>();
             string path = editor.GetScriptPath();
 
             string folderPath = Path.GetDirectoryName(path);
-            string[] assetGUID = AssetDatabase.FindAssets("", new[] { folderPath });
+            string parentFolder = Path.GetDirectoryName(folderPath);
+            string[] assetGUID = AssetDatabase.FindAssets("", new[] { parentFolder }).Where(x => x != AssetDatabase.AssetPathToGUID(folderPath)).ToArray();
 
             foreach (string s in assetGUID)
             {
                 string ext = Path.GetExtension(AssetDatabase.GUIDToAssetPath(s));
                 string oldPath = AssetDatabase.GUIDToAssetPath(s);
+
                 File.Move(oldPath, folderPath + "/ARToDelete" + s + ext);
 
                 assetsToDelete.Add(folderPath + "/ARToDelete" + s + ext);
-                metasToDelete.Add(oldPath + ".meta");
+                // metasToDelete.Add(oldPath + ".meta");
             }
         }
 
         public static void DeleteAssets()
         {
-            AssetDatabase.DeleteAsset(outputPath);
+            AssetDatabase.DeleteAsset(packagePath);
 
-            foreach (string s in metasToDelete)
-            {
-                if (File.Exists(s))
-                {
-                    File.Delete(s);
-                }
-            }
+            // foreach (string s in metasToDelete)
+            // {
+            //     if (File.Exists(s))
+            //     {
+            //         File.Delete(s);
+            //     }
+            // }
 
             foreach (string s in assetsToDelete)
             {
